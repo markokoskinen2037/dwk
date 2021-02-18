c26c63097a0f5ee457ba3d73ad70fb0b88ce35ca
Marko K <markokoskinen2037@gmail.com>
2021-02-18 16:17:05 +0200

    added frontend

--- main_application/index.js
+++ main_application/index.js
@@ -1,12 +1,15 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 const port = 3000
 const VERSION = "v2"
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+app.use(express.static(path.join(__dirname, "/frontti/build")))
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/frontti/build", "index.html"))
 })
 
 app.listen(port, () => {
-  console.log(`${VERSION} Server started in port NNNN`)
+  console.log(`${VERSION} Server started in port ${port}`)
 })
