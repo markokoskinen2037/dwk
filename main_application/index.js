2a8d0bb84fc8c15e4bd6844cf1f07d55db969daa
Marko K <markokoskinen2037@gmail.com>
2021-02-24 16:09:04 +0200

    1.07

--- main_application/index.js
+++ main_application/index.js
@@ -3,11 +3,23 @@ const path = require("path")
 const app = express()
 const port = 3000
 const VERSION = "v2"
+const crypto = require("crypto")
+var RANDOM_STRING = crypto.randomBytes(20).toString("hex")
+
+const getStringWithTimeStamp = () => {
+  return `${new Date().toLocaleTimeString()} ${RANDOM_STRING}`
+}
 
 app.use(express.static(path.join(__dirname, "/frontti/build")))
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/frontti/build", "index.html"))
+})
+
+app.get("/status", (req, res) => {
+  const data = getStringWithTimeStamp()
+  console.log(data)
+  res.send(data).end()
 })
 
 app.listen(port, () => {
