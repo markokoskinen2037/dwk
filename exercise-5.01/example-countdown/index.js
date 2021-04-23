80d8996e639d3809a7e3cb7b2b0f30ed0bce0b4c
Marko Koskinen <markokoskinen2037@gmail.com>
2021-04-23 12:52:57 +0300

    pagecloner v1

--- exercise-5.01/example-countdown/index.js
+++ exercise-5.01/example-countdown/index.js
@@ -1,5 +1,70 @@
-const time = Number(process.argv[2]) || 10
+const scrape = require("website-scraper")
+const PuppeteerPlugin = require("website-scraper-puppeteer")
+const path = require("path")
+const express = require("express")
+const URL_TO_CLONE = "http://example.org" || process.env.URL_TO_CLONE
 
-if (time < 3) console.log('Time is coming to an end!')
+const letsgo = async () => {
+  console.log("scraping...")
 
-console.log('Time:', time)
+  try {
+    await scrape({
+      // Provide the URL(s) of the website(s) that you want to clone
+      // In this example, you can clone the Our Code World website
+      urls: [URL_TO_CLONE],
+      // Specify the path where the content should be saved
+      // In this case, in the current directory inside the ourcodeworld dir
+      directory: path.resolve(__dirname, "public"),
+      // Load the Puppeteer plugin
+      plugins: [
+        new PuppeteerPlugin({
+          launchOptions: {
+            // If you set  this to true, the headless browser will show up on screen
+            headless: true,
+          } /* optional */,
+          scrollToBottom: {
+            timeout: 10000,
+            viewportN: 10,
+          } /* optional */,
+        }),
+      ],
+    })
+
+    console.log("scraped.")
+
+    const app = express()
+    const port = 3000
+
+    app.use(express.static("public"))
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!")
+    })
+
+    app.listen(port, () => {
+      console.log(`Serving the closed website at http://localhost:${port}`)
+    })
+  } catch (error) {
+    console.log("failed to scrape")
+    console.log(error)
+
+    const app = express()
+    const port = 3000
+
+    app.get("/", (req, res) => {
+      res.send("Failed to scrape")
+    })
+
+    app.listen(port, () => {
+      console.log(`Serving errorpage at http://localhost:${port}`)
+    })
+  }
+}
+
+letsgo()
+
+// const time = Number(process.argv[2]) || 10
+
+// if (time < 3) console.log('Time is coming to an end!')
+
+// console.log('Time:', time)
